22d66eb6d862f1f9468b51be628f5dafc185c35a
nathan <nathankdgray@gmail.com>
2023-09-20 20:34:14 -0700

    comments (learning)
    - updated comments to refresh my mind on basic functions

--- main analyser.py
+++ main analyser.py
@@ -1,32 +1,34 @@
-from tkinter import Tk, filedialog  # import tkinter
-from textblob import TextBlob   #import textblob
+from tkinter import Tk, filedialog          # import tkinter
+from textblob import TextBlob           #import textblob
 
+
+# use an Enter input to proceed with analyzing
 input("Press Enter to open .txt finder...")
 
-# Function to open a filedialog and import the selected file path
+# function to open a filedialog and import the selected file path
 def open_file_dialog():
-    root = Tk()
-    root.withdraw()
+    root = Tk()         # access tkinter library
+    root.withdraw()         # hide/make the library invisible
 
-    # Open filepath and accept/open the file that the user selected
+    # open filepath and accept/open the file that the user selected
     file_path = filedialog.askopenfilename()
 
     # Run an analysis on the .txt file that was selected
     if file_path:
-        perform_sentiment_analysis(file_path)
+        perform_sentiment_analysis(file_path)           # perform an analysis on the txt file that was selected
 
 # Funtion to perform an analysis on the .txt file that was selected  
-def perform_sentiment_analysis(file_path):
-    with open(file_path, "r") as file:
-        text = file.read()
-        blob = TextBlob(text)
-        polarity = blob.sentiment.polarity
-        subjectivity = blob.sentiment.subjectivity
+def perform_sentiment_analysis(file_path):          # perform the analysis on the text file
+    with open(file_path, "r") as file:          # store the file path
+        text = file.read()          # read the text in the selected file
+        blob = TextBlob(text)           # process the text data
+        polarity = blob.sentiment.polarity          # run a polarity test on the selected .txt file
+        subjectivity = blob.sentiment.subjectivity          # run a subjectivity test on the selected .txt file
 
-# To print results from analysis
-        print ("Friendliness Analysis Results: ")
-        print ("Polarity: ",polarity)
-        print ("Subjectivity: ",subjectivity)
+# print results from analysis
+        print ("Friendliness Analysis Results: ")           # print the "Analysis Result" to the screen
+        print ("Polarity: ",polarity)           # print the "Polarity" results to the screen
+        print ("Subjectivity: ",subjectivity)           # print the "Subjectivity" results to the screen
 
-
+# open file dialog to select file (ignore)
 open_file_dialog()
